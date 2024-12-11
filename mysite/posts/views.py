03d476050b2bb625ccb4d5ad1777462e40fa2106
정필성 <ibetter.kr@gmail.com>
2024-12-11 21:44:23 +0900

    기능 수정

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -7,11 +7,6 @@ from .forms import PostCreateForm, PostUpdateForm
 
 # 게시글 등록
 def posts_create(request):
-    context = {
-        'form': PostCreateForm(),
-        'message_class': 'col-12'
-    }
-    
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         
@@ -21,10 +16,10 @@ def posts_create(request):
             messages.success(request, '게시글이 등록되었습니다.')
             return redirect("posts:read", post_id=post.id)
         else:
-            context['form'] = form
             messages.error(request, '게시글 등록에 실패했습니다.')
 
-    return render(request, 'posts/create.html', context)
+    form = PostCreateForm()
+    return render(request, 'posts/create.html', {'form': form})
 
 # 게시글 보기
 def posts_read(request, post_id):
@@ -33,13 +28,13 @@ def posts_read(request, post_id):
 
 # 게시글 수정
 def posts_update(request, post_id):
-    post = get_object_or_404(Posts, id=post_id)
+    post = get_object_or_404(Posts, id=post_id)    
     
     if request.method == 'POST':
-        form = PostUpdateForm(request.POST, instance=post)
+        form = PostUpdateForm(request.POST)
         
         if form.is_valid():
-            if form.cleaned_data['passwd'] == post.passwd:
+            if form.cleaned_data['password'] == post.password:
                 post = form.save(commit=False)
                 post.save()
                 messages.success(request, '게시글이 수정되었습니다.')
@@ -47,19 +42,18 @@ def posts_update(request, post_id):
             else:
                 messages.error(request, '비밀번호가 일치하지 않습니다.')
         else:
-            for field_name, error_messages in form.errors.items():
-                messages.error(request, f"{form.fields[field_name].label}: {error_messages[0]}")
-    else:
-        form = PostUpdateForm(instance=post)
-        
+            messages.error(request, '게시글 수정에 실패했습니다.')
+
+    form = PostUpdateForm(instance=post)
     return render(request, 'posts/update.html', {'form': form})
 
 # 게시글 삭제
 def posts_delete(request, post_id):
     post = get_object_or_404(Posts, id=post_id)
+    password = request.POST.get('password')
     
     if request.method == 'POST':
-        if request.POST['passwd'] == post.passwd:
+        if password == post.password:
             post.delete()
             messages.success(request, '게시글이 삭제되었습니다.')
             return redirect('posts:list')
@@ -69,5 +63,5 @@ def posts_delete(request, post_id):
 
 # 게시글 목록
 def posts_list(request):
-    posts = Posts.objects.all().order_by('-id')
+    posts = Posts.objects.all().order_by('-created_at')
     return render(request, 'posts/list.html', {'posts': posts})
